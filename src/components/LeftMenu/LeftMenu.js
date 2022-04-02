96e281fc57c42a6023cfaa922057a6a0e8da194a
user <ramizazain@gmail.com>
2022-04-02 23:33:53 +0530

    Menu added to left

--- src/components/LeftMenu/LeftMenu.js
+++ src/components/LeftMenu/LeftMenu.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import './LeftMenu.css';
-
+import {BiFoodMenu} from 'react-icons/bi';
 function LeftMenu() {
   return (
     <div className='containerLeft'>
-
+      <div className='menuContainer'>
+        <BiFoodMenu width={20} height={20} color={'#3cc274'} />
+        <h4 className='menuText'>MENU</h4>
+      </div>
     </div>
   )
 }
